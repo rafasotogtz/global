af189e09a8f3cbb3e36eaa630813f7f1f44d3412
rafaelsotogtz <58016474+rafasotogtz@users.noreply.github.com>
2020-10-21 16:12:47 -0700

    ok
    ok

--- correos.php
+++ correos.php
@@ -3,11 +3,12 @@
     // esto es al correo al que se enviara el mensaje
     $nombre = $_POST['nombre'];
     $email = $_POST['email'];
+    $mensaje = $_POST['servicio'];
 
     $header = $email;
     $mensajeCompleto = $mensaje . "\nAtentamente: " .$nombre;
 
-    mail($destinatario, $mensajeCompleto, $header);
+    mail($destinatario, $mensajeCompleto, $header, );
     echo "<script>alert('correo enviado exitosamente')</script>";
     echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
 
